dbfdd100fbd8ceba9ad5a769bcd469909b60af6a
AsalaToson <153466617+AsalaToson@users.noreply.github.com>
2024-06-22 01:37:32 +0300

    Update parent.blade.php

--- resources/views/admin/parent.blade.php
+++ resources/views/admin/parent.blade.php
@@ -14,7 +14,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css"
           integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA=="
           crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="{{ asset('../../css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('../../css/style_3.css') }}">
 
     @yield('css')
 
@@ -45,10 +45,10 @@
                     <li><a href="{{route('patient.index2')}}">All Patient</a></li>
                 </ul>
             </li>
-            <li><a href="#" class="toggle-link"><i class="fas fa-users"></i>Staff</a>
+            <li><a href="#" class="toggle-link"><i class="fas fa-users"></i>Stuff</a>
                 <ul class="sublist">
-                    <li><a href="{{route('admins.create')}}">Add Staff</a></li>
-                    <li><a href="{{route('admins.index')}}">All Staff</a></li>
+                    <li><a href="{{route('admins.create')}}">Add Stuff</a></li>
+                    <li><a href="{{route('admins.index')}}">All Stuff</a></li>
                 </ul>
             </li>
 
@@ -91,49 +91,65 @@
 
     <div class="content">
         <div class="nav">
-                <nav class="navbar navbar-expand-lg ">
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
 
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav ml-auto">
-                            <li class="nav-item">
-                                <a class="nav-link" href="#"><i class="fas fa-sun"></i></a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#"><i class="fas fa-moon"></i></a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#"><i class="fa fa-search"></i></a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#"><i class="fas fa-bell"></i></a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <i class="fas fa-user"> {{Auth::guard('admin')->user()->name}}  </i>
+            <nav class="navbar navbar-expand-lg">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto" >
+                        <li class="nav-item">
+                            <a class="nav-link" href="#" style="padding-top: 18px;"><i class="fa-solid fa-toggle-on" id="dark"></i>
+                            </a>
+                        </li>
+
+
+                        <li class="nav-item">
+                            <button class="nav-link btn btn-link" id="searchIcon" style="padding-top: 16px;"><i class="fa fa-search"></i></button>
+                        </li>
+
+                        <li class="nav-item dropdown" style="padding-top: 10px;">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" >
+                                <i class="fa fa-bell"></i>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown" style="width: 350px;" >
+                                <p style="text-align: center;"> you have 2 notification</p><hr>
+                                <a class="dropdown-item" href="#" style="height: 40px;"><i class="fa-solid fa-circle-exclamation"></i> you have new analysis request </a>
+                                <a class="dropdown-item" href="#" style="height: 40px;"><i class="fa-solid fa-circle-exclamation"></i> update your profile</a>
+                            </div>
+                        </li>
+                        <li class="nav-item-dropdown" >
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown2" role="button" data-toggle="dropdown" aria-haspopup="true" >
+                                <i class="fas fa-user" ></i>
+                                {{Auth::guard('admin')->user()->name}}
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown" style="height: 150px; width: 200px;">
+                                <a class="dropdown-item" href="{{route('admin profile.index')}}" ><i class="fa fa-user" aria-hidden="true"></i>
+                                    Profile</a>
+                                <a class="dropdown-item" href="#" ><i class="fa fa-user" aria-hidden="true"></i>
+                                    My schedule</a>
+                                <a class="dropdown-item" href="#" ><i class="fa fa-cog" aria-hidden="true"></i>
+                                    Settings</a>
+
+                                {{--             <a class="dropdown-item" href="#" ><i class="fa-solid fa-right-from-bracket"></i>--}}
+                                {{--               logout</a>--}}
+
+                                <form id="logout-form" action="{{ route('logout.admin') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+
+                                <a class="dropdown-item" href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                    <i class="fa-solid fa-right-from-bracket"></i> logout
                                 </a>
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="#">Settings</a>
 
-                                    <a class="dropdown-item" href="{{route('admin profile.index')}}">Profile</a>
-                                    <form id="logout-form" action="{{ route('logout.admin') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
+                            </div>
 
-                                    <a class="dropdown-item" href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                        <i class="fa-solid fa-right-from-bracket"></i> logout
-                                    </a>                            </div>
+                        </li>
+                    </ul>
+                </div>
 
-                                {{--                                <a class="dropdown-item" href="edit profile">Profile</a>--}}
-
-
-
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
+            </nav>
 
 
             </div>
@@ -179,4 +195,3 @@
 @yield('js')
 </body>
 </html>
-
